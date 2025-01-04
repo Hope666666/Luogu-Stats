b492df5b4d4b0ce8bcd8a87a464bdaf0d8dd5a08
Hope <2310639991@qq.com>
2025-01-04 17:08:17 +0800

    unified code format

--- src/common.js
+++ src/common.js
@@ -1,59 +1,59 @@
 const anafanafo = require('anafanafo');
 
 const NAMECOLOR = {
-  "Gray": "#bbbbbb",
-  "Blue": "#0e90d2",
-  "Green": "#5eb95e",
-  "Orange": "#e67e22",
-  "Red": "#e74c3c",
-  "Purple": "#9d3dcf",
-  "Cheater": "#ad8b00"
+    "Gray": "#bbbbbb",
+    "Blue": "#0e90d2",
+    "Green": "#5eb95e",
+    "Orange": "#e67e22",
+    "Red": "#e74c3c",
+    "Purple": "#9d3dcf",
+    "Cheater": "#ad8b00"
 }
 
 class Card {
-  constructor({
-    width = 450,
-    height = 250,
-    title = "",
-    body = "",
-    titleHeight = 25,
-    hideTitle = false,
-    css = "",
-    darkMode = "",
-    paddingX = 25,
-    paddingY = 35,
-    hideBorder = false,
-  }) {
-    this.width = width;
-    this.height = height;
-    this.titleHeight = titleHeight;
-    this.title = title;
-    this.body = body;
-    this.hideTitle = hideTitle;
-    this.css = css;
-    this.darkMode = darkMode;
-    this.paddingX = paddingX;
-    this.paddingY = paddingY;
-    this.hideBorder = hideBorder;
-  }
+    constructor({
+        width = 450,
+        height = 250,
+        title = "",
+        body = "",
+        titleHeight = 25,
+        hideTitle = false,
+        css = "",
+        darkMode = "",
+        paddingX = 25,
+        paddingY = 35,
+        hideBorder = false,
+    }) {
+        this.width = width;
+        this.height = height;
+        this.titleHeight = titleHeight;
+        this.title = title;
+        this.body = body;
+        this.hideTitle = hideTitle;
+        this.css = css;
+        this.darkMode = darkMode;
+        this.paddingX = paddingX;
+        this.paddingY = paddingY;
+        this.hideBorder = hideBorder;
+    }
 
-  render() {
-    const cardSize = {
-      width: this.width + 2*this.paddingX,
-      height: this.height + 2*this.paddingY,
-    };
-    if(!this.hideTitle) cardSize.height += this.titleHeight;
+    render() {
+        const cardSize = {
+            width: this.width + 2 * this.paddingX,
+            height: this.height + 2 * this.paddingY,
+        };
+        if (!this.hideTitle) cardSize.height += this.titleHeight;
 
-    const bgColor = this.darkMode?"#444444":"#fffefe";
-    let borderColor = "";
-    if(!this.hideBorder) borderColor = this.darkMode?"#444444":"#E4E2E2";
+        const bgColor = this.darkMode ? "#444444" : "#fffefe";
+        let borderColor = "";
+        if (!this.hideBorder) borderColor = this.darkMode ? "#444444" : "#E4E2E2";
 
-    return `
+        return `
       <svg xmlns="http://www.w3.org/2000/svg" width="${cardSize.width}" height="${cardSize.height}" viewBox="0 0 ${cardSize.width} ${cardSize.height}" fill="none">
         <style>
-          .text { font: 400 11px 'Segoe UI', Ubuntu, Sans-Serif; fill: ${this.darkMode?"#fffefe":"#333333"} }
-          .title {fill: ${this.darkMode?"#fffefe":"#333333"}}
-          .line { stroke:${this.darkMode?"#666666":"#dddddd"}; stroke-width:1 }
+          .text { font: 400 11px 'Segoe UI', Ubuntu, Sans-Serif; fill: ${this.darkMode ? "#fffefe" : "#333333"} }
+          .title {fill: ${this.darkMode ? "#fffefe" : "#333333"}}
+          .line { stroke:${this.darkMode ? "#666666" : "#dddddd"}; stroke-width:1 }
           ${this.css}
         </style>
         <rect x="0.5" y="0.5" rx="4.5" height="99%" stroke="${borderColor}" width="99%" fill="${bgColor}" stroke-opacity="1" />
@@ -67,7 +67,7 @@ class Card {
           ${this.body}
         </g>
       </svg>`;
-  }
+    }
 }
 
 function ToType(num) {
@@ -92,18 +92,18 @@ function ToType(num) {
  * @param {Object} option 其余选项
  */
 const renderError = (e, option) => {
-  const css = `.t {font: 600 18px 'Microsoft Yahei UI'; fill: #e74c3c;}`
-  const text = `<text class="t" dominant-baseline="text-before-edge">${e}</text>`
-  return new Card({
-    width: 500,
-    height: 23,
-    hideTitle: true,
-    css,
-    body: text,
-    paddingY: 20,
-    paddingX: 20,
-    ...option,
-  }).render();
+    const css = `.t {font: 600 18px 'Microsoft Yahei UI'; fill: #e74c3c;}`
+    const text = `<text class="t" dominant-baseline="text-before-edge">${e}</text>`
+    return new Card({
+        width: 500,
+        height: 23,
+        hideTitle: true,
+        css,
+        body: text,
+        paddingY: 20,
+        paddingX: 20,
+        ...option,
+    }).render();
 };
 
 /**
@@ -113,13 +113,13 @@ const renderError = (e, option) => {
  * @returns {string} ccf badge的svg字符串
  */
 const renderCCFBadge = (level, x) => {
-  const ccfColor = (ccf) => {
-    if(ccf >= 3 && ccf <= 5) return "#5eb95e";
-    if(ccf >= 6 && ccf <= 7) return "#3498db";
-    if(ccf >= 8) return "#f1c40f";
-    return null;
-  }
-  return `
+    const ccfColor = (ccf) => {
+        if (ccf >= 3 && ccf <= 5) return "#5eb95e";
+        if (ccf >= 6 && ccf <= 7) return "#3498db";
+        if (ccf >= 8) return "#f1c40f";
+        return null;
+    }
+    return `
   <svg xmlns="http://www.w3.org/2000/svg" x="${x}" y="-14" width="18" height="18" viewBox="0 0 18 18" fill="${ccfColor(level)}" style="margin-bottom: -3px;">
     <path d="M16 8C16 6.84375 15.25 5.84375 14.1875 5.4375C14.6562 4.4375 14.4688 3.1875 13.6562 2.34375C12.8125 1.53125 11.5625 1.34375 10.5625 1.8125C10.1562 0.75 9.15625 0 8 0C6.8125 0 5.8125 0.75 5.40625 1.8125C4.40625 1.34375 3.15625 1.53125 2.34375 2.34375C1.5 3.1875 1.3125 4.4375 1.78125 5.4375C0.71875 5.84375 0 6.84375 0 8C0 9.1875 0.71875 10.1875 1.78125 10.5938C1.3125 11.5938 1.5 12.8438 2.34375 13.6562C3.15625 14.5 4.40625 14.6875 5.40625 14.2188C5.8125 15.2812 6.8125 16 8 16C9.15625 16 10.1562 15.2812 10.5625 14.2188C11.5938 14.6875 12.8125 14.5 13.6562 13.6562C14.4688 12.8438 14.6562 11.5938 14.1875 10.5938C15.25 10.1875 16 9.1875 16 8ZM11.4688 6.625L7.375 10.6875C7.21875 10.8438 7 10.8125 6.875 10.6875L4.5 8.3125C4.375 8.1875 4.375 7.96875 4.5 7.8125L5.3125 7C5.46875 6.875 5.6875 6.875 5.8125 7.03125L7.125 8.34375L10.1562 5.34375C10.3125 5.1875 10.5312 5.1875 10.6562 5.34375L11.4688 6.15625C11.5938 6.28125 11.5938 6.5 11.4688 6.625Z">
     </path>
@@ -136,31 +136,31 @@ const renderCCFBadge = (level, x) => {
  * @param {number} progressWidth 柱状图的长度
  */
 const renderChart = (datas, labelWidth, progressWidth) => { //(label, color, height, num) => {
-  let chart = "";
-  let maxNum = datas.reduce((a, b) => Math.max(a, b.data), 0);
-  maxNum = (parseInt((maxNum-1) / 100) + 1) * 100;
+    let chart = "";
+    let maxNum = datas.reduce((a, b) => Math.max(a, b.data), 0);
+    maxNum = (parseInt((maxNum - 1) / 100) + 1) * 100;
 
-  for(let i = 0; i < datas.length; ++i) {
-    const width = (datas[i].data+1) / (maxNum+1) * progressWidth;
-    chart += `
-    <g transform="translate(0, ${i*30})">
+    for (let i = 0; i < datas.length; ++i) {
+        const width = (datas[i].data + 1) / (maxNum + 1) * progressWidth;
+        chart += `
+    <g transform="translate(0, ${i * 30})">
       <text x="0" y="15" class="text">${datas[i].label}</text>
       <text x="${width + labelWidth + 10}" y="15" class="text">${ToType(datas[i].data)}</text>
       <rect height="11" fill="${datas[i].color}" rx="5" ry="5" x="${labelWidth}" y="5" width="${width}"></rect>
     </g>
     `
-  }
+    }
 
-  const bodyHeight = datas.length * 30 + 10;
-  const dw = progressWidth / 4;
-  let coordinate = "";
-  for(let i = 0; i <= 4; ++i) {
-    coordinate += `
+    const bodyHeight = datas.length * 30 + 10;
+    const dw = progressWidth / 4;
+    let coordinate = "";
+    for (let i = 0; i <= 4; ++i) {
+        coordinate += `
     <line x1="${labelWidth + dw * i}" y1="0" x2="${labelWidth + dw * i}" y2="${bodyHeight - 10}"  class="line"/>
     <text x="${labelWidth + dw * i - (i == 0 ? 3 : 5)}" y="${bodyHeight}"  class="text">${ToType(maxNum * i / 4)}</text>
     `;
-  }
-  return coordinate + chart;
+    }
+    return coordinate + chart;
 }
 
 /**
@@ -172,10 +172,10 @@ const renderChart = (datas, labelWidth, progressWidth) => { //(label, color, hei
  * @param {string} rightTop 右上角的标签（展示总数）
  */
 const renderNameTitle = (name, color, ccfLevel, title, cardWidth, rightTop) => {
-  const nameLength = anafanafo(name)/10*1.8;
-  const nameColor = NAMECOLOR[color];
+    const nameLength = anafanafo(name) / 10 * 1.8;
+    const nameColor = NAMECOLOR[color];
 
-  return `
+    return `
   <g transform="translate(0, 0)" font-family="Verdana, Microsoft Yahei" text-rendering="geometricPrecision" font-size="18">
     <text x="0" y="0" fill="${nameColor}" font-weight="bold" textLength="${nameLength}">
       ${name}
@@ -190,12 +190,12 @@ const renderNameTitle = (name, color, ccfLevel, title, cardWidth, rightTop) => {
   </g>`;
 }
 
-module.exports = { 
-  NAMECOLOR,
-  Card,
-  renderError,
-  renderCCFBadge,
-  renderChart,
+module.exports = {
+    NAMECOLOR,
+    Card,
+    renderError,
+    renderCCFBadge,
+    renderChart,
     renderNameTitle,
-  ToType,
+    ToType,
 };
--- src/stats-card.js
+++ src/stats-card.js
@@ -1,9 +1,9 @@
 const axios = require("axios");
 let { uid, client_id, maxConcurrency } = require("../setting.js");
 const {
-  Card,
-  renderError,
-  renderChart,
+    Card,
+    renderError,
+    renderChart,
     renderNameTitle,
     ToType,
 } = require("./common.js");
@@ -26,7 +26,7 @@ const Basic = {
 async function fetchStats(id) {
     const stats = Basic;
     let cnt = 1;
-    let flag = 0; 
+    let flag = 0;
     const allRecords = [];  // 用于存储所有页面的记录
 
     // 发起请求的函数
@@ -49,7 +49,7 @@ async function fetchStats(id) {
         } catch (error) {
             console.error('Request failed', error);
             stats.Error = 1;
-         //   console.log(stats.Error);
+            //   console.log(stats.Error);
             return null;
         }
     }
@@ -136,61 +136,60 @@ async function fetchStats(id) {
 }
 
 const renderSVG = (stats, options) => {
-  const {
-    name,
+    const {
+        name,
         color,
         ccfLevel,
-      total,
-      hideInfo,
-      Error
-  } = stats;
+        total,
+        hideInfo,
+        Error
+    } = stats;
 
-  const {
-    hideTitle,
-    darkMode,
-    cardWidth = 500,
-  } = options || {};
+    const {
+        hideTitle,
+        darkMode,
+        cardWidth = 500,
+    } = options || {};
 
     if (Error) {
         return renderError("出现了一些问题，可能请求过于频繁，提出ISSUE或稍后再试")
     }
-  if(hideInfo) {
-    return renderError("用户开启了“完全隐私保护”，获取数据失败");
-  }
+    if (hideInfo) {
+        return renderError("用户开启了“完全隐私保护”，获取数据失败");
+    }
 
-  const paddingX = 25;
-  const labelWidth = 90;  //柱状图头部文字长度
-  const progressWidth = cardWidth - 2*paddingX - labelWidth - 60; //500 - 25*2(padding) - 90(头部文字长度) - 60(预留尾部文字长度)，暂时固定，后序提供自定义选项;
+    const paddingX = 25;
+    const labelWidth = 90;  //柱状图头部文字长度
+    const progressWidth = cardWidth - 2 * paddingX - labelWidth - 60; //500 - 25*2(padding) - 90(头部文字长度) - 60(预留尾部文字长度)，暂时固定，后序提供自定义选项;
 
-  const datas = [
-    {label: "C++", color:"#bfbfbf", data: total[2]+total[3]+total[4]+total[11]+total[12]+total[27]+total[28]},
-    {label: "Pascal", color:"#fe4c61", data: total[1]},
-    {label: "Python", color:"#f39c11", data: total[7]+total[25]},
-    {label: "Java", color:"#ffc116", data: total[8]+total[33]},
-      { label: "Rust", color: "#52c41a", data: total[15]},
-    {label: "Go", color: "#3498db", data: total[14]},
-    {label: "Haskell", color:"#9d3dcf", data: total[19]},
-      { label: "OCaml", color: "#0e1d69", data: total[30] },
+    const datas = [
+        { label: "C++", color: "#bfbfbf", data: total[2] + total[3] + total[4] + total[11] + total[12] + total[27] + total[28] },
+        { label: "Pascal", color: "#fe4c61", data: total[1] },
+        { label: "Python", color: "#f39c11", data: total[7] + total[25] },
+        { label: "Java", color: "#ffc116", data: total[8] + total[33] },
+        { label: "Rust", color: "#52c41a", data: total[15] },
+        { label: "Go", color: "#3498db", data: total[14] },
+        { label: "Haskell", color: "#9d3dcf", data: total[19] },
+        { label: "OCaml", color: "#0e1d69", data: total[30] },
     ]
     datas.sort(function (a, b) {
         return b.data - a.data;
     })
-    for (let i = datas.length-1; i >= 0; i--)
-    {
+    for (let i = datas.length - 1; i >= 0; i--) {
         if (datas[i].data == 0) datas.pop();
     }
-  const totalSum = total.reduce((a, b) => a + b);
+    const totalSum = total.reduce((a, b) => a + b);
     const body = renderChart(datas, labelWidth, progressWidth);
     const title = renderNameTitle(name, color, ccfLevel, "的代码语言", cardWidth, `已通过: ${ToType(totalSum)}`);
 
-  return new Card({
-    width: cardWidth - 2*paddingX,
-    height: datas.length*30 + 10,
-    hideTitle,
-    darkMode,
-    title,
-    body,
-  }).render();
+    return new Card({
+        width: cardWidth - 2 * paddingX,
+        height: datas.length * 30 + 10,
+        hideTitle,
+        darkMode,
+        title,
+        body,
+    }).render();
 }
 
 module.exports = { fetchStats, renderSVG }
