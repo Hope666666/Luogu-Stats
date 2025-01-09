84796e3415a52a09fe479e348405feef709434a0
Hope <2310639991@qq.com>
2025-01-09 21:06:38 +0800

    to:show object details
    res->res[0]

--- src/stats-card.js
+++ src/stats-card.js
@@ -47,7 +47,7 @@ async function fetchStats(id) {
                 },
             });
             if (res.data.code !== 200) {
-                throw new Error(res);
+                throw new Error(res[0]);
             }
             return res.data.currentData.records.result;
         } catch (error) {
@@ -120,7 +120,7 @@ async function fetchStats(id) {
                     });
 
                     if (res.data.code !== 200) {
-                        throw new Error(res);
+                        throw new Error(res[0]);
                     }
 
                     if (JSON.stringify(res.data.currentData.records.result[0]) == '[]') stats.hideInfo = 1;
